1020a0ba582e19af7afa3c729182069890d864b4
subahan ali <subahanislam523@gmail.com>
2025-01-26 18:46:13 +0600

    added to by conditional statement nested if else

---
+++ Module2/conditional_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if (tk > 100)
+    {
+        printf("basay jabo");
+    }
+    else
+    {
+        printf("bsy jabo na re");
+    }
+
+    return 0;
+}
